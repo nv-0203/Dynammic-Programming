9b9affe8f7aea7436df025b974169b57da14abd9
Nufail Vhora <nufailvhora03@gmail.com>
2024-01-14 22:26:55 +0530

    .

--- 1A_01Knapsack.cpp
+++ 1A_01Knapsack.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+
+int KnapSack(int val[], int wt[], int n, int W)
+{
+    // array to store final result
+    // dp[i] stores the profit with KnapSack capacity "i"
+    int dp[W + 1];
+ 
+    // initially profit with 0 to W KnapSack capacity is 0
+    memset(dp, 0, sizeof(dp));
+ 
+    // iterate through all items
+    for (int i = 0; i < n; i++)
+        // traverse dp array from right to left
+        for (int j = W; j >= wt[i]; j--)
+            dp[j] = max(dp[j], val[i] + dp[j - wt[i]]);
+    /*above line finds out maximum of  dp[j](excluding ith
+      element value) and val[i] + dp[j-wt[i]] (including ith
+      element value and the
+      profit with "KnapSack capacity - ith element weight")
+    */
+    return dp[W];
+}
 
 static int t[100][100];
 
